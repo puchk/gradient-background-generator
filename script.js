664c7ec1fbdec8b60c9bd1730f236594f017d959
puchk <kpuchalski@gmail.com>
2018-05-08 22:25:04 -0700

    adds working random colors button

--- script.js
+++ script.js
@@ -2,11 +2,29 @@ var color1 = document.querySelector("#color1");
 var color2 = document.querySelector("#color2");
 var backgroundText = document.querySelector("#backgroundText");
 var body = document.body;
+var randomButton = document.querySelector("#randomButton");
+
 
 function setGradient() {
- body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value +")";
+ body.style.background = `linear-gradient(to right,${color1.value},${color2.value})`;
  backgroundText.textContent = body.style.background;
 }
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+
+
+function randomNumber() {
+	return Math.floor(Math.random()*256);
+}
+
+function randomColor() {
+	return `rgb(${randomNumber()},${randomNumber()},${randomNumber()})`;
+}
+
+function getRandomColors() {
+	body.style.background = `linear-gradient(to right,${randomColor()}, ${randomColor()})`;
+	backgroundText.textContent = body.style.background;
+}
+
+randomButton.addEventListener("click", getRandomColors);
